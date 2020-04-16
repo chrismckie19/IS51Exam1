d69a1a592df92861e9c319baad4522b0c5ae4319
chrismckie19 <60371451+chrismckie19@users.noreply.github.com>
2020-04-15 19:53:30 -0700

    added exam 1 components

--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -12,6 +12,8 @@ import { Http } from '@angular/http';
 export class OrdersComponent implements OnInit {
 
   orders: Array<any> = [];
+  name: string = '';
+  errorMessage = '';
 
   constructor(
     private router: Router,
@@ -21,7 +23,92 @@ export class OrdersComponent implements OnInit {
   }
 
   async ngOnInit() {
-
+    this.loadSavedItems();
   }
 
+  loadSavedItems(){
+    this.orders = [{
+      'pid': '1',
+      'image':'assets/sm_android.jpeg',
+      'description': 'Android',
+      'price': 150.00,
+      'quantity': 2
+    }, {
+      'pid': '2',
+      'image':'assets/sm_iphone.jpeg',
+      'description': 'IPhone',
+      'price': 200.00,
+      'quantity': 1
+    }, {
+      'pid': '3',
+      'image':'assets/sm_windows.jpeg',
+      'description': 'Windows Phone',
+      'price': 110.00,
+      'quantity': 2
+    }];
+  }
+
+  delete(index: number) {
+    this.orders.splice(index, 1);
+  }
+
+  addItem(item: string) {
+    switch(item) {
+      case 'Android':
+        this.orders.unshift({
+          'pid': '1',
+          'image':'assets/sm_android.jpeg',
+          'description': 'Android',
+          'price': 150.00,
+          'quantity': 1
+        });
+        break;
+
+      case 'Iphone':
+        this.orders.unshift({
+          'pid': '2',
+          'image':'assets/sm_iphone.jpeg',
+          'description': 'IPhone',
+          'price': 200.00,
+          'quantity': 1
+        });
+        break;
+          
+      case 'Windows Phone':
+        this.orders.unshift({
+          'pid': '3',
+          'image':'assets/sm_windows.jpeg',
+          'description': 'Windows Phone',
+          'price': 110.00,
+          'quantity': 1
+        });
+        break;
+    }
+  }
+  submit() {
+    const commaIndex = this.name.indexOf(', ');
+    
+    let error = false;
+    
+    if (this.name === '') {
+      this.errorMessage = 'Name must not be empty!';
+      error = true;
+    }
+    else if(commaIndex === -1) {
+      this.errorMessage = 'Name must have a comma!';
+      error = true;
+    }
+    if (!error) {
+      const firstName = this.name.slice(commaIndex + 1, this.name.length);
+      const lastName = this.name.slice(0, commaIndex);
+      const fullName = firstName + ' ' + lastName;
+    }
+    else {
+      this.flexModal.openDialog('error-modal');
+    }
+  }
+    
+
+
 }
+
